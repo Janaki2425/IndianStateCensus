5b84967de186a7f128445de355b1dba985832012
Janaki2425 <96913266+Janaki2425@users.noreply.github.com>
2022-02-09 19:00:01 +0530

    csv adapter factory

---
+++ CSVAdapterFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using IndianStatesCensusAnalyser.POCO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStatesCensusAnalyser
+{
+    internal class CSVAdapterFactory
+    {
+        public Dictionary<string, CensusDTO> LoadCsvData(CensusAnalyser.Country country, string csvFilePath, string dataheaders)
+        {
+            switch (country)
+            {
+                case (CensusAnalyser.Country.INDIA):
+                    return new IndianCensusAdapter().LoadCensusData(csvFilePath, dataheaders);
+               
+                default:
+                    throw new CensusAnalyserException("No such country", CensusAnalyserException.ExceptionType.NO_SUCH_COUNTRY);
+            }
+        }
+    }
+}
+
+            
+        
+    
+
